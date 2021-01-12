f277b6fe147d44e4d48f21cdbebc085d3f0c40f6
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2021-01-11 23:52:55 -0800

    revised build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 "use strict";
-const express = require('express');
-const app = express();
-const port = 9000;
-app.get('/', (req, res) => res.send('hello world'));
-app.listen(port);
-console.log(`[app] : http://localhost:${port}`);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const compression_1 = __importDefault(require("compression"));
+const apollo_server_express_1 = require("apollo-server-express");
+const database_1 = require("./database");
+const graphql_1 = require("./graphql");
+const mount = (app) => __awaiter(void 0, void 0, void 0, function* () {
+    const db = yield database_1.connectDatabase();
+    const PORT = process.env.PORT || 3000;
+    app.use(body_parser_1.default.json({ limit: '2mb' }));
+    app.use(cookie_parser_1.default(process.env.SECRET));
+    app.use(compression_1.default());
+    app.use(express_1.default.static(`${__dirname}/the-shortlist-client`));
+    app.get('/*', (_req, res) => {
+        res.sendFile(`${__dirname}/the-shortlist-client/index.html`);
+    });
+    const server = new apollo_server_express_1.ApolloServer({
+        typeDefs: graphql_1.typeDefs,
+        resolvers: graphql_1.resolvers,
+        context: ({ req, res }) => ({ db, req, res }),
+    });
+    server.applyMiddleware({ app, path: '/api' });
+    app.listen(PORT);
+    console.log(`[app] : http://localhost:${PORT}`);
+});
+mount(express_1.default());
